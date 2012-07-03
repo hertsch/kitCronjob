c15b645c04f212c2210a7f143c2c0c687bf7d16f
hertsch <ralf.hertsch@phpmanufaktur.de>
2012-07-03 08:34:05 +0200

    Changed precheck, updated header informations

--- precheck.php
+++ precheck.php
@@ -2,19 +2,16 @@
 
 /**
  * kitCronjob
- * 
+ *
  * @author Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
- * @link http://phpmanufaktur.de
- * @copyright 2012 - phpManufaktur by Ralf Hertsch
- * @license http://www.gnu.org/licenses/gpl.html GNU Public License
- * @version $Id$
- * 
- * FOR VERSION- AND RELEASE NOTES PLEASE LOOK AT INFO.TXT!
+ * @link https://addons.phpmanufaktur.de/kitCronjob
+ * @copyright 2012 phpManufaktur by Ralf Hertsch
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
  */
 
 // include class.secure.php to protect this file and the whole CMS!
-if (defined('WB_PATH')) {    
-    if (defined('LEPTON_VERSION')) include(WB_PATH.'/framework/class.secure.php'); 
+if (defined('WB_PATH')) {
+    if (defined('LEPTON_VERSION')) include(WB_PATH.'/framework/class.secure.php');
 } else {
     $oneback = "../";
     $root = $oneback;
@@ -23,10 +20,10 @@ if (defined('WB_PATH')) {
         $root .= $oneback;
         $level += 1;
     }
-    if (file_exists($root.'/framework/class.secure.php')) { 
-        include($root.'/framework/class.secure.php'); 
+    if (file_exists($root.'/framework/class.secure.php')) {
+        include($root.'/framework/class.secure.php');
     } else {
-        trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", 
+        trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!",
                 $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
     }
 }
@@ -35,21 +32,16 @@ if (defined('WB_PATH')) {
 $PRECHECK['WB_VERSION'] = array('VERSION' => '2.8', 'OPERATOR' => '>=');
 $PRECHECK['PHP_VERSION'] = array('VERSION' => '5.2.0', 'OPERATOR' => '>=');
 $PRECHECK['WB_ADDONS'] = array(
-    'dbconnect_le'	=> array('VERSION' => '0.66', 'OPERATOR' => '>='),
-    'dwoo' => array('VERSION' => '0.11', 'OPERATOR' => '>='),
-		'manufaktur_i18n' => array('VERSION' => '0.10', 'OPERATOR' => '>=')
+    'manufaktur_config'	=> array('VERSION' => '0.14', 'OPERATOR' => '>='),
 );
 
 global $database;
-$sql = "SELECT `value` FROM `".TABLE_PREFIX."settings` WHERE `name`='default_charset'";
-$result = $database->query($sql);
-if ($result) {
-  $data = $result->fetchRow(MYSQL_ASSOC);
-  $PRECHECK['CUSTOM_CHECKS'] = array(
-      'Default Charset' => array(
-          'REQUIRED' => 'utf-8',
-          'ACTUAL' => $data['value'],
-          'STATUS' => ($data['value'] === 'utf-8')
-      )
-  );
-}
+$SQL = "SELECT `value` FROM `".TABLE_PREFIX."settings` WHERE `name`='default_charset'";
+$charset = $database->get_one($SQL, MYSQL_ASSOC);
+$PRECHECK['CUSTOM_CHECKS'] = array(
+	'Default Charset' => array(
+		'REQUIRED' => 'utf-8',
+		'ACTUAL' => $charset,
+		'STATUS' => ($charset === 'utf-8')
+	)
+);
